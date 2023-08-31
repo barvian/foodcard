9c85614f89a2e44b12f1e271277ca63703f750e8
Maxwell Barvian <max@barvian.me>
2023-08-31 10:31:01 -0500

    Update to actual fluid plugin
    not npm linked version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 import plugin from 'tailwindcss/plugin'
 import containerQueryPlugin from '@tailwindcss/container-queries'
-import { fluidCorePlugins, buildFluidExtract, defaultScreensInRems } from '@fluid.style/tailwind'
+import { fluidCorePlugins, buildFluidExtract, defaultScreensInRems } from '@fluidstyle/tailwindcss-plugin'
 const { '2xl': _, ...screens } = defaultScreensInRems
 
 export default {
